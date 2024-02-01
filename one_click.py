835d4d45cef8257b83bd012df1c5020dd2d397be
Neoathenian <jaimerbaron@gmail.com>
2024-02-01 14:55:40 +0100

    deleted git pull

--- one_click.py
+++ one_click.py
@@ -117,27 +117,7 @@ def install_webui():
 
 def update_requirements(initial_installation=False):
     print("initial_installation",initial_installation)
-    # Create .git directory if missing
-    if not os.path.isdir(os.path.join(script_dir, ".git")):
-        git_creation_cmd = 'git init -b main && git remote add origin https://github.com/oobabooga/text-generation-webui && git fetch && git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main && git reset --hard origin/main && git branch --set-upstream-to=origin/main'
-        run_cmd(git_creation_cmd, environment=True, assert_success=True)
-
-    files_to_check = [
-        'start_linux.sh', 'start_macos.sh', 'start_windows.bat', 'start_wsl.bat',
-        'update_linux.sh', 'update_macos.sh', 'update_windows.bat', 'update_wsl.bat',
-        'one_click.py'
-    ]
-
-    before_pull_hashes = {file_name: calculate_file_hash(file_name) for file_name in files_to_check}
-    run_cmd("git pull --autostash", assert_success=True, environment=True)
-    after_pull_hashes = {file_name: calculate_file_hash(file_name) for file_name in files_to_check}
-
-    # Check for differences in installation file hashes
-    for file_name in files_to_check:
-        if before_pull_hashes[file_name] != after_pull_hashes[file_name]:
-            print_big_message(f"File '{file_name}' was updated during 'git pull'. Please run the script again.")
-            exit(1)
-
+    
     # Extensions requirements are installed only during the initial install by default.
     # That can be changed with the INSTALL_EXTENSIONS environment variable.
     install = initial_installation
